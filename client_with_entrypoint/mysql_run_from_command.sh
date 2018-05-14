adf8f8d03ae5702db9de3cbf491c008194cc14d1
vittyz <vittyz@gmail.com>
2018-05-14 11:49:13 +0700

    Update mysql_run_from_command.sh

--- client_with_entrypoint/mysql_run_from_command.sh
+++ client_with_entrypoint/mysql_run_from_command.sh
@@ -1,8 +1,8 @@
 docker run -it --rm -v `pwd`/sql:/sql mysqlclient \
-    10.1.23.153 \
+    10.11.123.153 \
     occdbs_own \
     rpCQfX83p9jprHZUzFtz \
-    occdbs_db_test \
+    db_test \
     "select * from core_config_data"
 
 
