6177e22ac08c59e227759d165398b4f707531a5a
vittyz <vittyz@gmail.com>
2018-05-14 11:44:02 +0700

    Update rundocker.sh

--- client_with_entrypoint/rundocker.sh
+++ client_with_entrypoint/rundocker.sh
@@ -1,3 +1,8 @@
-docker run -it --rm -v /usr/home/spd/woravitp/mysql/sql:/sql mysqlclient 10.1.23.153 occdbs_own rpCQfX83p9jprHZUzFtz occdbs_db_test "select * from core_config"
+docker run -it --rm -v `pwd`/sql:/sql mysqlclient 
+    10.1.23.153 \
+    occdbs_own \
+    rpCQfX83p9jprHZUzFtz \
+    occdbs_db_test \
+    "select * from core_config_data"
 
 
