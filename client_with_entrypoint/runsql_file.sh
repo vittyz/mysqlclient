f817ae814871642314fc4fc22b710eaa635e0ff0
vittyz <vittyz@gmail.com>
2018-05-14 11:45:39 +0700

    Update runsql_file.sh

--- client_with_entrypoint/runsql_file.sh
+++ client_with_entrypoint/runsql_file.sh
@@ -1,1 +1,6 @@
-docker run -it --rm -v `pwd`/sql:/sql mysqlclient 10.1.23.153 occdbs_own rpCQfX83p9jprHZUzFtz occdbs_db_test file /sql/select.sql
+docker run -it --rm -v `pwd`/sql:/sql mysqlclient \
+  10.1.23.153 \
+  occdbs_own \
+  rpCQfX83p9jprHZUzFtz \
+  occdbs_db_test \
+  file /sql/select.sql
