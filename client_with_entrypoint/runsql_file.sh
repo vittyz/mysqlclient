3382653180c29362505679d4ac872502acf0837c
vittyz <vittyz@gmail.com>
2018-05-14 11:41:49 +0700

    Update runsql_file.sh

--- client_with_entrypoint/runsql_file.sh
+++ client_with_entrypoint/runsql_file.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -v /usr/home/spd/woravitp/mysql/sql:/sql mysqlclient 10.1.23.153 occdbs_own rpCQfX83p9jprHZUzFtz occdbs_db_test file /sql/select.sql
+docker run -it --rm -v `pwd`/sql:/sql mysqlclient 10.1.23.153 occdbs_own rpCQfX83p9jprHZUzFtz occdbs_db_test file /sql/select.sql
